31a4219be5bff7bea1ab64fc5897fc2e1cdf5d24
Amit Garg <09s.amitgarg@gmail.com>
2022-06-03 19:32:00 +0530

    Update 2

--- Package.swift
+++ Package.swift
@@ -17,15 +17,16 @@ let package = Package(
             targets: ["GlobalPackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            .package(url: "https://github.com/microsoft/plcrashreporter.git", from: "1.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GlobalPackage",
-            dependencies: []),
+            dependencies: [
+                            .product(name: "CrashReporter", package: "plcrashreporter")
+                        ]),
         .testTarget(
             name: "GlobalPackageTests",
             dependencies: ["GlobalPackage"]),
